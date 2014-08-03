44afd7069d277b629bc8298821f88efde7a24fba
cmberryau <cmberryau@gmail.com>
2014-08-03 23:21:10 +0200

    started io class for reading files

---
+++ src/utility/io.cpp
@@ -0,0 +1,21 @@
+//
+//  io.cpp
+//  Renderer
+//
+//  Created by Christopher Berry on 03/08/2014.
+//  Copyright (c) 2014 Christopher Berry. All rights reserved.
+//
+
+#include "utility/io.hpp"
+
+#include <fstream>
+
+namespace Renderer
+{
+	const char * IO::ReadFile(const char * file_path)
+	{
+		
+
+		return nullptr;
+	}
+}
---
+++ src/utility/io.hpp
@@ -0,0 +1,20 @@
+//
+//  io.hpp
+//  Renderer
+//
+//  Created by Christopher Berry on 03/08/2014.
+//  Copyright (c) 2014 Christopher Berry. All rights reserved.
+//
+
+namespace Renderer
+{
+	class IO
+	{
+		public:
+			static const char * ReadFile(const char * file_path);
+
+		protected:
+			IO();
+			~IO();
+	};
+}
