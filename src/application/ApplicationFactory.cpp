9042feb0c9cb36876408be9d0c8a1e5202480f7e
cmberryau <cmberryau@gmail.com>
2014-09-17 14:44:52 +0200

    application stubs added

---
+++ src/application/ApplicationFactory.cpp
@@ -0,0 +1,19 @@
+//
+//  ApplicationFactory.cpp
+//  Renderer
+//
+//  Created by Christopher Berry on 16/09/2014.
+//  Copyright (c) 2014 Christopher Berry. All rights reserved.
+//
+
+#include "ApplicationFactory.hpp"
+
+namespace Renderer
+{
+	void ApplicationFactory::FromFile(std::string & file_path)
+	{
+
+
+		return;
+	}
+}
