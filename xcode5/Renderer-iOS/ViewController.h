9e849aa3b0977b0edaf0720782a6860f2e782e61
cmberryau <cmberryau@gmail.com>
2014-10-14 17:24:52 +0200

    setting up for iOS

---
+++ xcode5/Renderer-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Renderer-iOS
+//
+//  Created by Christopher Berry on 14/10/2014.
+//  Copyright (c) 2014 Christopher Berry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xcode5/Renderer-iOS/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Renderer-iOS
+//
+//  Created by Christopher Berry on 14/10/2014.
+//  Copyright (c) 2014 Christopher Berry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
